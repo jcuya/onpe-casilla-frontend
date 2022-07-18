8e4254c2fdc89b0e11ab20791a4cf52a342832be
Edy Garay <edy.garay@nexus-virtual.net>
2022-07-18 15:18:50 -0500

    cambiis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'frontend_create_casilla';
+
+  @ViewChild('principal') elementView!: ElementRef;
+
+  contentHeight!: number;
+
+
+  ngOnInit(){
+    
+  }
+
+  ngAfterViewInit() {
+    this.contentHeight = this.elementView.nativeElement.offsetHeight;
+
+    if( this.contentHeight >= 370){
+
+    }
+
+    console.log("tamaño", this.contentHeight)
+}
+
 }
