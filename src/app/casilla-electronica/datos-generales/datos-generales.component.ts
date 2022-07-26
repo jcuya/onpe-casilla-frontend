18af21f947f4a7ba79ed55082ce3f1249c5975c7
Edy Garay <edy.garay@nexus-virtual.net>
2022-07-25 20:52:29 -0500

    cambios

--- src/app/casilla-electronica/datos-generales/datos-generales.component.ts
+++ src/app/casilla-electronica/datos-generales/datos-generales.component.ts
@@ -98,6 +98,7 @@ export class DatosGeneralesComponent implements OnInit {
 
 
     this.siguientePaso();
+   
     /*if (!this.formGroup.valid) {
       this.formGroup.markAllAsTouched()
       return
