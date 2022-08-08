cacfbca827f346b161e86bce2ce0d035b50f065f
Edy Garay <edy.garay@nexus-virtual.net>
2022-08-08 18:09:48 -0500

    cambios

--- src/app/casilla-electronica/foto-dni/foto-dni.component.ts
+++ src/app/casilla-electronica/foto-dni/foto-dni.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { FileUploadValidators } from '@iplab/ngx-file-upload';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { FileUploadControl, FileUploadValidators } from '@iplab/ngx-file-upload';
 import { Subscription } from 'rxjs';
 import { requestGlobal } from 'src/app/core/dto/request';
 import { CasillaService } from 'src/app/core/services/casilla.service';
+import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'app-foto-dni',
@@ -15,12 +17,14 @@ export class FotoDniComponent implements OnInit {
   @Output() completedStep = new EventEmitter<any>()
   @Output() previousStep = new EventEmitter<any>()
 
+  maxsize_ = 31200;
+
 
   observableRequestSubscription!: Subscription;
   requestSave: requestGlobal = new requestGlobal();
 
   formGroup!: FormGroup;
-  constructor( private formBuilder: FormBuilder,private casillaService: CasillaService) {
+  constructor( private formBuilder: FormBuilder,private casillaService: CasillaService,private dialog: MatDialog) {
 
     this.observableRequestSubscription = casillaService.casilla$.subscribe(
       (requestSave: requestGlobal) => {
@@ -30,11 +34,16 @@ export class FotoDniComponent implements OnInit {
     );
    }
 
+
+
+
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       files: this.filesControl
     });
   }
+
+
 
  
   handleArchivoAgregado(event: any) {
@@ -48,6 +57,7 @@ export class FotoDniComponent implements OnInit {
 
   continuar() {
     if(!this.formGroup.valid){
+    
       this.formGroup.markAllAsTouched()
       return
     }
@@ -59,12 +69,48 @@ export class FotoDniComponent implements OnInit {
   }
 
 
-  public filesControl = new FormControl(null, [
+  public filesControl = new FormControl( null, [
     Validators.required,
-    FileUploadValidators.accept(['file_extension|image/*']),
+    FileUploadValidators.accept(['image/*']),
     FileUploadValidators.filesLimit(1),
-    FileUploadValidators.fileSize(1048576 * 10),
+    FileUploadValidators.fileSize(this.maxsize_),
    // this.noWhitespaceValidator,
   ]);
+
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.formGroup.controls ;
+  }
+
+
+  validatorImage(){
+    const Image = this.f.files.value[0]
+
+    if(Image.size >= this.maxsize_){
+      this.dialog.open(AlertDialogComponent, {
+        disableClose: true,
+        hasBackdrop: true,
+        data: {cabecera : 'Error!' ,messages: ['La imagen debe tener un peso máximo de 3Mb']}
+      })
+      this.filesControl.setValue([]);
+
+    }
+
+    const type = Image.type.substr(0,5)
+
+    if(type !== "image"){
+      this.dialog.open(AlertDialogComponent, {
+        disableClose: true,
+        hasBackdrop: true,
+        data: {cabecera : 'Error!' ,messages: ['El archivo debe ser de tipo imagen  (PNG, JPG o JPEG)']}
+      })
+      this.filesControl.setValue([]);
+    }
+    return;
+   // this.f.files.setValue(null)
+    //  this.formGroup.get('files')?.setValue(null);
+
+
+   }
 
 }
